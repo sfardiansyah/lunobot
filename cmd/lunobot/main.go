6d38b28427f7a5cf5356fdf3c5291164a7571b2d
Syahrul Ardiansyah <syahrul.findi@gmail.com>
2019-02-16 15:27:56 +0700

    Use replyf

--- cmd/lunobot/main.go
+++ cmd/lunobot/main.go
@@ -35,7 +35,7 @@ func defaultHandler(m *tbot.Message) {
 	// if len(m.Vars["new_chat_members"]) > 0 {
 	// 	m.Reply("Halo!")
 	// }
-	m.Reply(m.Vars["new_chat_members"])
+	m.Replyf("%+v\n", m)
 }
 
 func startHandler(m *tbot.Message) {
